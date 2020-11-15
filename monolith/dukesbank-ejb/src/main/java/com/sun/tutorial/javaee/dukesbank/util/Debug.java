19d13568280f9568f4ea38ced00956e704890fca
Rafael Marins <rafa.marins@gmail.com>
2020-11-15 20:06:50 -0300

    monolith jee dukes bank app

---
+++ monolith/dukesbank-ejb/src/main/java/com/sun/tutorial/javaee/dukesbank/util/Debug.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2007 Sun Microsystems, Inc.
+ * All rights reserved.  You may not modify, use,
+ * reproduce, or distribute this software except in
+ * compliance with  the terms of the License at:
+ * http://developer.sun.com/berkeley_license.html
+ */
+
+
+package com.sun.tutorial.javaee.dukesbank.util;
+
+import weblogic.i18n.logging.NonCatalogLogger;
+
+
+/**
+ * This class makes it easier to print out debug statements.
+ * The Debug.print statements are printed to System.err
+ * if debuggingOn = true.
+ */
+public final class Debug {
+    public static final boolean debuggingOn = true;
+
+    private static NonCatalogLogger logger = new NonCatalogLogger(Debug.class.getName());
+
+    public static final void print(String msg) {
+        if (debuggingOn) {
+            logger.debug("Debug: " + msg);
+        }
+    }
+
+    public static final void print(
+        String msg,
+        Object object) {
+        if (debuggingOn) {
+            logger.debug("Debug: " + msg);
+            logger.debug("       " + object.getClass().getName());
+        }
+    }
+}
---
+++ monolith/dukesbank-ejb/src/main/java/weblogic/i18n/logging/NonCatalogLogger.java
@@ -0,0 +1,20 @@
+package weblogic.i18n.logging;
+
+public class NonCatalogLogger {
+
+    public NonCatalogLogger() {
+
+    }
+
+    public NonCatalogLogger(String logName) {
+
+    }
+
+    public void info(String msg) {
+
+    }
+
+    public void debug(String msg) {
+
+    }
+}
